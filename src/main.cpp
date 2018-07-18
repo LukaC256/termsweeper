5fe988ee7da3b422c2a13eb6dce964fb8e7c1049
LukaCPP <36575504+LukaCPP@users.noreply.github.com>
2018-07-18 21:42:31 +0200

    Added Command-Line Options

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,38 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* args[])
 {
+	int iXSize = 8;
+	int iYSize = 8;
+	int iMines = 10;
+	for (size_t a = 0; a < argc; a++)
+	{
+		char* arg = args[a];
+		if (*arg == '-')
+		{
+			switch(arg[1])
+			{
+			case 'x':
+				a++;
+				iXSize = atoi(args[a]);
+				break;
+			case 'y':
+				a++;
+				iYSize = atoi(args[a]);
+				break;
+			case 'm':
+				a++;
+				iMines = atoi(args[a]);
+				break;
+			default:
+				cout << "Unrecognised argument " << arg[1] << "!\n";
+				break;
+			}
+		}
+	}
 	CMap Map;
-	Map.Init(CVector(10, 10), 10);
+	Map.Init(CVector(iXSize, iYSize), iMines);
 	cout << "Map Initialized!\n";
 	Map.printMap();
 	Map.Quit();
